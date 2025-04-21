4c3fcd82dae0c2c8b2e700a733087542d826c780
GitHub Action <action@github.com>
2025-04-21 21:40:14 +0000

    Bump version to 1.0.32 [skip ci]

--- instagram-single-story.user.js
+++ instagram-single-story.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram Single Story View
 // @namespace    https://github.com/eltoro0815/instagram-single-story-view
-// @version      1.0.31
+// @version      1.0.32
 // @description  Erzwingt die Einzelansicht für Instagram-Stories und verhindert die Karussell-Navigation
 // @author       eltoro0815
 // @match        https://www.instagram.com/stories/*
